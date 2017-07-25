670f66b2c2f0a9d8a03a7b0fa17e9da437deeda9
Leandro Martins <leandromarte@gmail.com>
2017-07-24 21:18:13 -0300

    Initial commit

---
+++ src/main/java/com/leandro/HotmartApplication.java
@@ -0,0 +1,12 @@
+package com.leandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmartApplication.class, args);
+	}
+}
